2244792e50f5e00fd23dc16cf1caaa4d28419f79
xstefank <xstefank122@gmail.com>
2019-11-05 12:21:39 +0100

    Clean up outdated quarkus project

--- src/main/java/io/xstefank/Constants.java
+++
@@ -1,8 +0,0 @@
-package io.xstefank;
-
-public class Constants {
-
-    public static final String WILDFLY_URL = "https://github.com/wildfly/wildfly";
-    
-    public static final String JBOSS_EAP7 = "https://github.com/jbossas/jboss-eap7";
-}
